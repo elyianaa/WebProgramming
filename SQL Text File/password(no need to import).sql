03745f3311ad6caa6f5168ec8982e63f9a368b94
Elyiana Amiera <93247774+elyianaa@users.noreply.github.com>
2023-01-03 12:07:06 +0800

    Add files via upload

---
+++ SQL Text File/password(no need to import).sql
@@ -0,0 +1,7 @@
+Medical Staff:
+Username: ms1, ms2, ms3
+Password: ms1@123, ms2@123, ms3@123
+
+User:
+Username: user1, user2, user3, user4, user5
+Password: user1@123, user2@123, user3@123, user4@123, user5@123
